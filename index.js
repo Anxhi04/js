7015db5bf37e07329733099676aaf5593cb1d50c
Anxhelika <krecianxhelika04@gmail.com>
2025-08-05 16:44:31 +0200

    Krijimi i butonit dark/light mode dhe funksionaliteti

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const togglebutton =document.getElementById('tooglebutton');
 const sidebar = document.getElementById('sidebar');
+const lightDarkButton = document.getElementById('light-dark');  
+
 
 function togglesidebar() {
     sidebar.classList.toggle('close');
@@ -9,4 +11,13 @@ function togglesidebar() {
 function togglesubmenu(button){
     button.nextElementSibling.classList.toggle('show');
     button.classList.toggle('rotate');
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    if(document.body.classList.contains('dark-mode')){
+        body.classList.remove('dark-mode');
+    }else{
+        body.classList.add('dark-mode');
+    }
 }
